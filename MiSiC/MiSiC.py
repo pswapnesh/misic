0b72147e2c43b1fae5b4e6e41ac04977d8612641
pswapnesh <50742608+pswapnesh@users.noreply.github.com>
2021-03-29 13:47:09 +0200

    Update MiSiC.py

--- MiSiC/MiSiC.py
+++ MiSiC/MiSiC.py
@@ -3,37 +3,14 @@ from tensorflow.keras.utils import get_file
 import matplotlib.pyplot as plt
 import numpy as np
 from skimage.transform import resize,rescale
-from skimage.util import random_noise,pad
+from skimage.util import random_noise
 from skimage.io import imread,imsave
-from skimage.filters import gaussian, laplace, threshold_otsu, median
+from skimage.filters import gaussian
 from skimage.feature import shape_index
-from skimage.feature import hessian_matrix, hessian_matrix_eigvals
 from MiSiC.utils import *
-from scipy.ndimage import gaussian_laplace
-
-
-# def pre_processing(im,scale):    
-#     tmp = unsharp_mask(im*1.0)
-#     tmp = adjust_gamma(tmp,0.1)
-#     tmp = normalize2max(rescale(tmp,scale))    
-#     return noise_profile(tmp)    
-
-def pre_processing(im,scale,noise_var = 0.0001):    
-    tmp = unsharp_mask(im*1.0)
-    #tmp = adjust_gamma(tmp,0.25)    
-    tmp = (rescale(im,scale))        
-    tmp = gaussian_laplace(tmp,sigma = 1.8)
-    tmp = 1-normalize2max(tmp)    
-    return noise_profile(tmp,noise_var*scale)
-
-def post_processing(y,orig_size):
-    
-    return resize(y[:,:,0],orig_size) >0.90
-    
 
 class MiSiC():
-    def __init__(self):
-        self.size = 256
+    def __init__(self,model_name = 'MiSiC/MiSiDC04082020.h5'):
         model_path = get_file(
             'misic_model',
             'https://github.com/pswapnesh/Models/raw/master/MiSiDC04082020.h5') ## 0721
@@ -41,30 +18,26 @@ class MiSiC():
         self.model.compile(optimizer='adam',
               loss='categorical_crossentropy',
               metrics=['accuracy'])
-    
+        self.size = 256        
+        
     def shapeindex_preprocess(self,im):
+        ''' apply shap index map at three scales'''
         sh = np.zeros((im.shape[0],im.shape[1],3))
         if np.max(im) ==0:
             return sh
-        pw = 15
-        im = pad(im,pw,'reflect')
-        sh = np.zeros((im.shape[0],im.shape[1],3))    
-        sh[:,:,0] = shape_index(im,1)
-        sh[:,:,1] = shape_index(im,1.5)
-        sh[:,:,2] = shape_index(im,2)     
+        
+        # pad to minimize edge artifacts
+        pw = 8
+        im = np.pad(im,pw,'reflect')            
+        sh[:,:,0] = shape_index(im,1)[pw:-pw,pw:-pw]
+        sh[:,:,1] = shape_index(im,1.5)[pw:-pw,pw:-pw]
+        sh[:,:,2] = shape_index(im,2)[pw:-pw,pw:-pw]
         #sh = 0.5*(sh+1.0)
-        return sh[pw:-pw,pw:-pw,:]
+        return sh
     
-    def segment(self,im,invert = False):
-        im = normalize2max(im)        
-        pw = 16
-        if invert:
-            im = 1.0-im
-        im = pad(im,pw,'reflect')
-        sh = self.shapeindex_preprocess(im)
-        
-        tiles,params = extract_tiles(sh,size = self.size,padding = 8)
-        
+    def segment(self,im,invert = False,exclude = 32):        
+        sh = self.shapeindex_preprocess(im)        
+        sh = -sh if invert else sh
+        tiles,params = extract_tiles(sh,size = self.size,exclude = exclude)               
         yp = self.model.predict(tiles)
-
-        return stitch_tiles(yp,params)[pw:-pw,pw:-pw,:]
+        return stitch_tiles(yp,params)
