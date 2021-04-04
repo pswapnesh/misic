3c4f1563ec9e726c9ad27f400a279fea196db167
pswapnesh <swapnesh.off@gmail.com>
2021-04-04 13:04:12 +0200

    v 0.0.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="MiSiC", # Replace with your own username
-    version="1.0.0",
+    version="1.0.1",
     author="S.Panigrahi",
     author_email="spanigrahi@imm.cnrs.fr",
     description="Microbe segmentation in dense colonies",
