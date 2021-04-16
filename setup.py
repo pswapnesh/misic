f234519471de49c0f1d0bd68d616df8876eea73d
pswapnesh <50742608+pswapnesh@users.noreply.github.com>
2021-04-16 11:24:40 +0200

    namechange

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="MiSiC", # Replace with your own username
+    name="misic", # Replace with your own username
     version="1.1.0",
     author="imm-lcb-iam-pswap-le",
     author_email="spanigrahi@imm.cnrs.fr",
